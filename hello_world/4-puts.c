268cb98741f15d327ab531f0b157746fbfffaf17
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-06 15:22:02 -0700

    1

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - function includes the following, uses put to put a string
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
