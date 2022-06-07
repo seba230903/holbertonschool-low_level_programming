ebc8c2b7fa9629a3ea77f5a71c778209fabd87d3
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-07 16:26:14 -0700

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Description: print all single digit numbers including zero
+ * Return: 0
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 48; p <= 57; p++)
+		putchar(p);
+	putchar(10);
+	return (0);
+}
