84ccd04dda61f05fe98237fe4ecd290bae40db02
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-07 17:30:55 -0700

    task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,10 +11,12 @@ int main(void)
 	for (a = 48; a <= 57; a++)
 	{
 		putchar(a);
-		if (a == 57)
-			putchar(10);
-		putchar(44);
-		putchar(32);
+		if (a != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
+	putchar(10);
 	return (0);
 }
