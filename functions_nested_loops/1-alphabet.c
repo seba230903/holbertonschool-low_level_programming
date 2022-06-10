5da5f52911e94d1b14dd73d990c90ee310a611a5
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-10 11:41:01 -0700

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * print_alphabet - print_alphabet
+ * Description: prints the alphabet followed by a new line
+ * Return: 0
+ */
+int print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar(10);
+}
