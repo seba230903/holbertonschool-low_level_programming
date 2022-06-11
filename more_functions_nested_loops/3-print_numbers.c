eaa1cac82786d778ebdb6a0eec4d82f9191500ad
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-11 15:55:17 -0700

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ * Description: prints from 0 to 9
+ * @a: num
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+		_putchar(a);
+	_putchar(10);
+}
