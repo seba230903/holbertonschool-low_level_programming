91b58b68588aae3f375428885f877cc657dac668
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-11 18:32:29 -0700

    task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main
+ * Description: prints from 1 to 100
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz");
+		else
+		{
+			if (a % 3 == 0)
+				printf("Fizz");
+			if (a % 5 == 0)
+				printf("Buzz");
+			if (a % 3 != 0 && a % 5 != 0)
+				printf("%d", a);	
+		}
+		if (a != 100)
+			printf(" ");
+	}
+	putchar(10);
+	return (0);
+}
