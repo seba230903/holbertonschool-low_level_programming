07bc8b560b318db432cc0ae11f86e3ea9497f462
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-15 14:23:38 -0700

    task 8

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - copy string
+ * Description: copies string pointed to by src, to dest
+ * @dest: string
+ * src: string
+ * Return: temp
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *temp = dest;
+
+	while (*src != 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = 0;
+	return (temp);
+}
