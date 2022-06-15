e1134daedf43c3ac50a547b54c4ebeeb669b4964
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-14 18:44:57 -0700

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#inlcude "main.h"
+/**
+ * swap_int - swaps values
+ * Description: swaps the values of two integers
+ * @a: first integer
+ * @b: second integer
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
