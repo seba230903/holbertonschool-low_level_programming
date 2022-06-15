7de3d6c1e58b341d503d9278f5c0f57fbfbc9278
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-14 19:02:32 -0700

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints string
+ * Description: prints a string, in reverse, followed by a new line
+ *@s: string
+ */
+void print_rev(char *s)
+{
+	int length = strlen(s);
+	int a = length - 1;
+
+	while (a >= 0)
+	{
+		_putchar(*(s + a));
+		a--;
+	}
+	_putchar(10);
+}	
