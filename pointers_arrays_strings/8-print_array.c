d8eaea5650640ddb7f1fa47622f4c5786327a46c
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-15 14:18:46 -0700

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - prints array
+ * Description: prints n elements of an array of integers, followed by a new line\
+ * @n: num
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	int limit = n - 1;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < limit)
+		{
+			printf(", ");
+		}
+	}
+	putchar(10);
+}
