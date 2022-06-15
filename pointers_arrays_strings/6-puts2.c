cbb0be95fff1e43ebe1620a924ab2eb73dbbb484
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-15 11:31:00 -0700

    task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - prints characters
+ * Description: prints every other character of a string, followed by a new line
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int a;
+	int b; strlen(str);
+
+	for (a = 0; a < b; a += 2)
+		_putchar((str[a]));
+	_putchar(10);
+}
