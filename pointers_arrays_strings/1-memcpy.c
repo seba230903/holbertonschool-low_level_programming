c11177f847e67a4098e5326dacb29483ceeb8719
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-19 15:03:42 -0700

    task 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory
+ * Description: copies memory area
+ * @dest: pointer
+ * @src: pointer
+ * @n: unsigned integer
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
