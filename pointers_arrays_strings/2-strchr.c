f66a412556fda2e8a27f47ee57ad487942ed10ec
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-19 15:26:45 -0700

    task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - locates character
+ * Description: locates a character in a string
+ * @s: pointer
+ * @c: chr
+ * Return: pointer to c or null
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+	char *j;
+
+	for (i = 0; s[i] != '\0' && s[i] != c; i++)
+	{
+		if (s[i] == c)
+			j = &s[i];
+		else
+			j = '\0';
+	}
+	return (j);
+}
