85f517d5c80ba065747cb9bac61ce10b45961909
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-21 17:12:18 -0700

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Description: prints its name, followed by a new line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
