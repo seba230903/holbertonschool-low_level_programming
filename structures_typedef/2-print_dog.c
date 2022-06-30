3194240733c0b281bc29d3723bbdc36e10b2c30b
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-30 16:33:03 -0700

    task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else 
-		printf("Name: %\n", d->name);
+		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nill)\n");
