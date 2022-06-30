f9177a569235efcab3ef2776483e6308559639a0
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-30 16:51:56 -0700

    task 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,18 +3,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * struct dog - struct dog
+ * struct dog - dog :)
  * Description: dog structure
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
