19c05d1299594fc06961e8ebb2aa0de6369dc0cc
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-30 18:00:22 -0700

    task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+/**
+ * new_dog - new dog
+ * Description: creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (!new_dog)
+		return (NULL);
+	new_dog->name = strdup(name);
+	if (!new_dog->name)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = strdup(owner);
+	if (!new_dog->owner)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->age = age;
+	return (new_dog);
+}
