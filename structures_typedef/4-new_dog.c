871b9f0acdff414743add26518cb593c35be9a72
seba230903 <lopezdominguez.seba@gmail.com>
2022-06-30 18:10:44 -0700

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -6,6 +6,7 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ * Return: NULL if faili
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -15,13 +16,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (!new_dog)
 		return (NULL);
 	new_dog->name = strdup(name);
-	if (!new_dog->name)
+	if (!(new_dog->name))
 	{
 		free(new_dog);
 		return (NULL);
 	}
 	new_dog->owner = strdup(owner);
-	if (!new_dog->owner)
+	if (!(new_dog->owner))
 	{
 		free(new_dog->name);
 		free(new_dog);
