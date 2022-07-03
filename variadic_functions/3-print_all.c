06f1107821fe4bae27af10a1028729a37518d09b
seba230903 <lopezdominguez.seba@gmail.com>
2022-07-03 16:53:43 -0700

    task 3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include "variadic_functions.h"
+/**
+ * print_all - prints anything
+ * Description: prints anything
+ * @format: list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	long unsigned int i = 0;
+
+	if (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(args, format);
+	while (i < strlen(format))
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case's':
+				printf("%s", va_arg(args, char *));
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (i != (strlen(format) - 1))
+			printf(", ");
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
