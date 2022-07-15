3e7523d9c850541881ae27146e5cb5a0590e49ca
seba230903 <lopezdominguez.seba@gmail.com>
2022-07-15 15:35:34 -0700

    task 5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees list
+ * Description: frees a listint_t list
+ * @head: pointer to list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *p;
+
+	while (*head != NULL)
+	{
+		p = (*head)->next;
+		free(*head);
+		(*head) = p;
+	}
+	(*head) = NULL;
+}
