f5a164a12cebd2cb156571da5ff27f901fac75bd
seba230903 <lopezdominguez.seba@gmail.com>
2022-07-15 16:39:28 -0700

    task 6 & 7

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes node
+ * Description: deletes the head node of a listint_t linked 
+ * list, and returns the head node’s data
+ * @head: pointer to list
+ * Return: head node's data
+ */
+int pop_listint(listint_t **head)
+{
+	int aux;
+	listint_t *p;
+
+	if (!head)
+		return (-1);
+	else
+	{
+		aux = (*head)->n;
+		p = (*head)->next;
+		free(*head);
+		*head = p;
+	}
+	return(aux);
+}
---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - returns the nth of a node
+ * Description: returns the nth node of a listint_t linked list
+ * @head: pointer to list
+ * @index: int
+ * Return: nth node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *p = head;
+	unsigned int i = 0;
+	
+	if (!head)
+		return(NULL);
+	while (i < index)
+	{
+		p = p->next;
+		i++;
+	}
+	return (p);
+}
