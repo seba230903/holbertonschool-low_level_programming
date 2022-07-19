d11682a6c08596e3221e9c1a2bdf16562f9fcad0
seba230903 <lopezdominguez.seba@gmail.com>
2022-07-19 16:57:24 -0700

    changed everything

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -16,13 +16,13 @@ void print_binary(unsigned long int n)
 	}
 	i = i - 1;
 	if (n == 0)
-		_putchar('0');
+		putchar('0');
 	while (i >= 0)
 	{
 		if ((n >> i) & 1)
-			_putchar('1');
+			putchar('1');
 		else
-			_putchar('0');
+			putchar('0');
 		i--;
 	}
 }
