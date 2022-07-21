f1a8fddd19dba48c9d408a54a76e350cfdfe1f92
seba230903 <lopezdominguez.seba@gmail.com>
2022-07-20 17:04:55 -0700

    fix betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - creates a file
  * Description: creates a file
  * @filename: name of file
- * text_content: content of file
+ * @text_content: content of file
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
