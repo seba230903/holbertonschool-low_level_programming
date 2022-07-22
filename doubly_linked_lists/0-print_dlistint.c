29b4ad0db45331fd158984d19a32c2f1146e828c
seba230903 <lopezdominguez.seba@gmail.com>
2022-07-22 16:49:00 -0700

    task 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints elements of list
+ * Description: prints all the elements of a dlistint_t list
+ * @h: pointer to list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	unsigned int i = 0;
+	char *p = h;
+
+	while (p != NULL)
+	{
+		printf("%d", p->n);
+		p = p->next;
+		i++;
+	}
+	return (i);
+}
