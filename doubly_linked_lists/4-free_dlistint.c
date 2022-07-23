90f490778cdb240c97b5eb8a13cc4d196cd3858a
seba230903 <lopezdominguez.seba@gmail.com>
2022-07-22 18:07:55 -0700

    task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees list
+ * Description: frees a dlistint_t list
+ * @head: pointer to list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *p;
+
+	while (head != NULL)
+	{
+		p = head->next;
+		free(head);
+		head = p;
+	}
+	free(head);
+}	
