fa081d89adf9c6430830056d47e71f0d52a2efea
seba230903 <lopezdominguez.seba@gmail.com>
2022-08-20 10:54:34 -0700

    readme, main.h and task 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * Description: creates a hash table
+ * @int - size
+ * Return: pointer to newly created hash table or NULL if fail
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = NULL;
+	unsigned long int i = 0;
+
+	if (!size)
+		return (NULL);
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!table->array)
+	{
+		free(table);
+		return (NULL);
+	}
+	table->size = size;
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+	return (table);
+}
