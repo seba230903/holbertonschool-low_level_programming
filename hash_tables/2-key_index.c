2e8004a7c412e683e58142240be38807663efa59
seba230903 <lopezdominguez.seba@gmail.com>
2022-08-21 14:23:42 -0700

    task 2

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_table.h"
+/**
+ * key_index - gives index key
+ * Description: gives you the index of a key
+ * @key: char
+ * @size: int
+ * Return: index of key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = (hash_djb2(key));
+	return (index);
+}
