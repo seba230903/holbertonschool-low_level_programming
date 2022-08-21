e3180adee92464e00f93243af09156e92b515285
seba230903 <lopezdominguez.seba@gmail.com>
2022-08-21 14:26:03 -0700

    im dumb so i forgot the %

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,4 +1,4 @@
-#include "hash_table.h"
+#include "hash_tables.h"
 /**
  * key_index - gives index key
  * Description: gives you the index of a key
@@ -10,6 +10,6 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
 
-	index = (hash_djb2(key));
+	index = (hash_djb2(key) % size);
 	return (index);
 }
